7ae11a9e2bb6e5de994490dea51515fdcce572e2
Lizandro Antonio <codeversebackend@gmail.com>
2024-03-15 20:53:00 -0600

    ci: The migration of tables to the database was applied, and the MindFlow application was also created | Dev: Lizandro

---
+++ MindFlow_BackEnd/MindFlow/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MindflowConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'MindFlow'
--- MindFlow_BackEnd/MindFlow_BackEnd/settings.py
+++ MindFlow_BackEnd/MindFlow_BackEnd/settings.py
@@ -36,7 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',  
+     
+    # Add the MindFlow app to the list of installed apps
+    'MindFlow.apps.MindflowConfig', 
 ]
 
 MIDDLEWARE = [
