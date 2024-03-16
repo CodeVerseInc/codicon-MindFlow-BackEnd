e02a7b2f3adb60cc10aa5c72f053efc1584f3359
Lizandro Antonio <codeversebackend@gmail.com>
2024-03-16 14:27:52 -0600

    ci: Create Models, Views in Admin and configuration the rutes, In addition, the migrations of the models to the database were applied | Dev: Lizandro

--- MindFlow_BackEnd/MindFlow/admin.py
+++ MindFlow_BackEnd/MindFlow/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
+from django.contrib import admin 
+from .models import Users, Profile, Post, Comments
 
-# Register your models here.
+# Register your models here.  
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):  
+    fields = ['email', 'username', 'password'] 
+    list_display = ['email', 'username', 'password'] 
+
+@admin.register(Profile)    
+class ProfileAdmin(admin.ModelAdmin):  
+    fields = ['user', 'profile_photo', 'username_fk', 'description'] 
+    list_display = ['user', 'profile_photo', 'username_fk', 'description'] 
+
+@admin.register(Post)    
+class PostAdmin(admin.ModelAdmin): 
+    fields = ['profile_fk', 'title', 'content'] 
+    list_display = ['profile_fk', 'title', 'content'] 
+
+@admin.register(Comments)     
+class CommentsAdmin(admin.ModelAdmin): 
+    fields = ['profile_fk', 'content'] 
+    list_display = ['profile_fk', 'content']
+
--- MindFlow_BackEnd/MindFlow/models.py
+++ MindFlow_BackEnd/MindFlow/models.py
@@ -1,3 +1,64 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+# Create your models here. 
+# Solucionar problema de herencia de User
+class Users(models.Model):   
+    email = models.EmailField(unique=True) 
+    username = models.CharField(max_length=100, unique=True)  
+    password = models.CharField(max_length=128, null=False)  
+
+    class Meta: 
+        db_table = 'users' 
+        verbose_name = 'User'
+        verbose_name_plural = 'Users' 
+        ordering = ['id'] 
+        unique_together = ['email', 'username', 'password'] 
+         
+    def __str__(self): 
+        return self.username
+  
+
+class Profile(models.Model):  
+    user = models.OneToOneField(Users, on_delete=models.CASCADE, related_name='profile') 
+    profile_photo = models.URLField(max_length=500)   
+    username_fk = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='profiles') 
+    description = models.TextField(max_length=500)
+       
+    class Meta: 
+        db_table = 'profile' 
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles' 
+        ordering = ['id'] 
+         
+    def __str__(self): 
+        return self.user.username
+
+class Post(models.Model): 
+    profile_fk = models.ForeignKey(Profile, on_delete=models.CASCADE) 
+    title = models.CharField(max_length=100) 
+    content = models.TextField(max_length=500)  
+     
+    class Meta: 
+        db_table = 'post' 
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts' 
+        ordering = ['id'] 
+         
+    def __str__(self): 
+        return self.title.content
+
+   
+class Comments(models.Model): 
+    profile_fk = models.ForeignKey(Profile, on_delete=models.CASCADE) 
+    post_fk = models.ForeignKey(Post, on_delete=models.CASCADE) 
+    content = models.TextField(max_length=500) 
+     
+    class Meta: 
+        db_table = 'comments' 
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments' 
+        ordering = ['id'] 
+         
+    def __str__(self): 
+        return self.content
