2f6184b50c293fef6b3443b2f379724650ac3cfe
Lizandro Antonio <codeversebackend@gmail.com>
2024-03-17 11:48:54 -0600

    ci: Changue the route the virtual env | Dev: Lizandro

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activar el entorno virtual
-source ./venv/Scripts/activate
+source venv\Scripts\activate.bat
 
 # Ir al directorio del proyecto Django
 cd ./MindFlow_BackEnd/MindFlow_BackEnd
