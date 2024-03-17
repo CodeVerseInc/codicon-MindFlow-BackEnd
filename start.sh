1ae15d3b4abb28b17304076cd9175ae9e5ef5b8e
Lizandro Antonio <codeversebackend@gmail.com>
2024-03-17 11:32:35 -0600

    ci: Add routing the virtual env | Dev: Lizandro

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activar el entorno virtual
-source .\venv\Scripts\activate
+source source ./venv/Scripts/activate
 
 # Ir al directorio del proyecto Django
 cd ./MindFlow_BackEnd/MindFlow_BackEnd
